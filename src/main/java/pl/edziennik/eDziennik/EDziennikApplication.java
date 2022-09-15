a823067b801b5f5826bdd5a1622fe5068c9e82bd
SaQie <Kamcio99@interia.com>
2022-09-15 21:10:00 +0200

    upload project on github

---
+++ src/main/java/pl/edziennik/eDziennik/EDziennikApplication.java
@@ -0,0 +1,12 @@
+package pl.edziennik.eDziennik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDziennikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDziennikApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/edziennik/eDziennik/EDziennikApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edziennik.eDziennik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDziennikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
