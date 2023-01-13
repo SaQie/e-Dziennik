0cbf77b95d1203998548eb9e1ac8cef792c23535
SaQie <Kamcio99@interia.com>
2023-01-13 19:32:51 +0100

    fixing resource problems

--- src/test/java/pl/edziennik/eDziennik/BaseTest.java
+++ src/test/java/pl/edziennik/eDziennik/BaseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.annotation.Propagation;
@@ -58,6 +59,7 @@ public class BaseTest {
     protected void clearDb(){
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator(false, false, "UTF-8", new ClassPathResource("/db/clearData.sql"));
         populator.execute(dataSource);
+        LocaleContextHolder.setDefaultLocale(new Locale("en_EN"));
     }
 
     protected final Long ROLE_ADMIN = 1L;
